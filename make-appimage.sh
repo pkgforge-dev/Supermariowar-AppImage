03c1991cba3a9c234428917382dad0bbf5a604b1
Link4Electronics <link4electronics@gmail.com>
2026-03-23 18:13:53 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/32x32/apps/smw.png
 export DESKTOP=/usr/share/applications/supermariowar.desktop
+export STARTUPWMCLASS=smw
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
