1c920b8376749f6c3754b0ce6435fc54332cbd26
Link4Electronics <link4electronics@gmail.com>
2026-03-23 18:35:22 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,9 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    libdecor \
-    libvorbis
+pacman -Syu --noconfirm libdecor
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
