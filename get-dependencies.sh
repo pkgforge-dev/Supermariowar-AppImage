ae05a05722f6b6a9a05d7a19e3ef8316a2834a9e
Link4Electronics <link4electronics@gmail.com>
2026-03-23 16:31:45 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,5 +16,5 @@ get-debloated-pkgs --add-common --prefer-nano
 PRE_BUILD_CMDS="sed -i '\|-DSMW_DATADIR:path=\"/var/lib/smw\"|d' ./PKGBUILD" make-aur-package supermariowar-git
 
 # If the application needs to be manually built that has to be done down here
-mkdir -p ./AppDir/bin
-#mv -v /var/lib/smw/* ./AppDir/bin
+mkdir -p ./AppDir/bin/data
+mv -v /usr/share/games/smw/* ./AppDir/bin/data
